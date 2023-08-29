ba1e399f4e19e2d3280df63ae66fee7e5827e9ef
LarsKarlsen82 <lpuk82@gmail.com>
2023-08-29 10:06:54 +0200

    update

---
+++ assets/js/functionsThree.js
@@ -0,0 +1,28 @@
+
+
+
+// her skal du skrive din anonyme funktion
+let myAnonFunc=myFunction("Hello world");
+
+
+// her kaldes, eller invokes din anonyme funktion gennem en variabel
+console.log(myAnonFunc);
+
+function myFunction(text) {
+    let somtext  = text
+
+
+return somtext;
+}
+
+
+// her skal du skrive din arrow funktion
+let myArrowFunc= "";
+
+myArrowFunc = function () {
+    return "Hello World!";
+}
+
+
+// her kaldes, eller invokes din arrow funktion gennem en variabel
+console.log(myArrowFunc());
---
+++ assets/js/functionsTwo.js
@@ -0,0 +1,12 @@
+let myNumber= 1;
+
+// her kaldes, eller invokes din funktion = helloWorld("hello world, med parameter");
+helloWorld(1);
+helloWorld(5);
+
+// her skal du skrive din funktion
+function helloWorld(ExtraNumber){
+
+    console.info(myNumber + ExtraNumber);
+
+}
